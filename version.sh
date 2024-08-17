46902f1c88bf31294dd4d6a17176f72c3ab63d94
mintsuki <mintsuki@protonmail.com>
2024-08-17 16:08:32 +0200

    Template backports

--- version.sh
+++ version.sh
@@ -1,19 +1,4 @@
 #! /bin/sh
-# version.sh: Script for identifying version based on git commits.
-# Copyright (C) 2023 streaksu
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 LC_ALL=C
 export LC_ALL
@@ -23,5 +8,22 @@ test -z "$srcdir" && srcdir=.
 
 cd "$srcdir"
 
-[ -f version ] || ( git describe --exact-match --tags $(git log -n1 --pretty='%h') 2>/dev/null || git log -n1 --pretty='%h' ) | sed 's/^v//g' | xargs printf '%s'
-[ -f version ] && ( cat version 2>/dev/null ) | xargs printf '%s'
+if test -f version; then
+    printf '%s' "$(cat version)"
+    exit 0
+fi
+
+if ! test -d .git || ! git log -n1 --pretty='%h' >/dev/null 2>&1; then
+    printf 'UNVERSIONED'
+    exit 0
+fi
+
+tmpfile="$(mktemp)"
+
+if ! git describe --exact-match --tags $(git log -n1 --pretty='%h') >"$tmpfile" 2>/dev/null; then
+    echo g$(git log -n1 --pretty='%h') >"$tmpfile"
+fi
+
+printf '%s' "$(sed 's/^v//g' <"$tmpfile")"
+
+rm -f "$tmpfile"
